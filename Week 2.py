811c864abf5cb2588b832628b14fbcbbf5189d2e
jaden-choi28 <jac112@ucsd.edu>
2026-07-13 16:38:06 -0700

    Week 2.py

--- Week 2.py
+++ Week 2.py
@@ -1,7 +1,7 @@
 import pandas as pd
 from pathlib import Path
 
-combined_files = Path(r"C:\Users\jjcho\OneDrive\Desktop\IDX Exchange Internship\concatenated py files")
+combined_files = Path(r"C:\Users\jjcho\OneDrive\Desktop\IDX Exchange Internship\Weekly csv files")
 
 #CSV files already filtered to PropertyType = Residential only
 listing_file = combined_files / "combined_listings_residential.csv"
@@ -25,10 +25,12 @@ print(f"The following are the unique property sub types for sold: {", ".join(uni
 
 
 #Summing all null values
+over_90_null_list = []
+
 listing_df_null = []
 listing_df_col = listing_df.columns
 listing_df_row_num = len(listing_df.index)
-listing_null_sum = 0
+
 
 #append all sums of nulls for each column in a list for listing
 for col in listing_df_col:
@@ -36,33 +38,40 @@ for col in listing_df_col:
     print(f"{'\033[37m'}{col} has {null_num} amount of nulls in its column for listing.\n")
     listing_df_null.append(null_num)
 
-    
-    if listing_df_null[listing_null_sum] > .9*len(listing_df.index):
-        print(f"{'\033[31m'}{col} has greater than 90% null values for listing.\n")
-    
-    listing_null_sum += 1
-
-
 
 sold_df_null = []
 sold_df_col = sold_df.columns
 sold_df_row_num = len(sold_df.index)
-sold_null_sum = 0
-
 #append all sums of nulls for each column in a list for sold
 for col in sold_df_col:
     null_num = sold_df[col].isnull().sum()
     print(f"{'\033[37m'}{col} has {null_num} amount of nulls in its column for sold.\n")
     sold_df_null.append(null_num)
 
-    if sold_df_null[sold_null_sum] > .9*len(sold_df.index):
-        print(f"{'\033[31m'}{col} has greater than 90% null values for sold.\n")
     
-    sold_null_sum += 1
 
-#Ask what columns are best to remove, remove manually or to remove with code?
+#Drop cols with over 90% nulls
+listing_null_percent = listing_df.isnull().mean()*100
+sold_null_percent = sold_df.isnull().mean()*100
+
+listing_drop_cols = listing_null_percent[listing_null_percent > 90].index
+sold_drop_cols = sold_null_percent[sold_null_percent > 90].index
+
+print(f"Listing columns to drop: {listing_drop_cols.tolist()}")
+print(f"Sold columns to drop: {sold_drop_cols.tolist()}")
+
+cleaned_listing_df = listing_df.drop(columns=listing_drop_cols)
+cleaned_sold_df = sold_df.drop(columns=sold_drop_cols)
+
+
 
 #numeric distribution summary
 for col in listing_df_col:
     if col in ["ClosePrice", "LivingArea", "DaysOnMarket"]:
         print(f"{'\033[32m'} Max for {col} is {listing_df[col].max()} {'\033[37m'} and {'\033[33m'} min is {listing_df[col].min()}.\n")
+        print(f"{'\033[32m'} Mean for {col} is {listing_df[col].mean()} {'\033[37m'} and {'\033[33m'} median is {listing_df[col].median()}.\n")
+
+
+#Save files
+cleaned_listing_df.to_csv(r"Directory", index=False)
+cleaned_sold_df.to_csv(r"Directory", index=False)
